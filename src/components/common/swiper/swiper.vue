8a789632679b12bf546334bfde10d05417704119
wzc3867 <799015924@qq.com>
2022-02-15 00:28:20 +0800

    写轮播样式

--- src/components/common/swiper/swiper.vue
+++ src/components/common/swiper/swiper.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
   <div style="overflow: hidden;">
-    <div class="swiper-box" :style="move">
+    <div class="swiper-box" ref="swiperBox">
     <img :src="item.image" v-for="(item, index) in imgswip" :key="index" ref="swipers" >
   </div>
   </div>
@@ -17,12 +17,15 @@ export default {
   mounted() {
     setTimeout(() =>{
       console.log(this.$refs.swipers);
-      this.staatTime()
+      this.handleDom();
+      this.staatTime();
     },300)
   },
   data () {
     return {
-      currentIndex:0
+      currentIndex:1,
+      swiperStyle:{},
+      totalWidth:0
     };
   },
   components: {},
@@ -35,10 +38,42 @@ export default {
 
   methods: {
     staatTime(){
-     
       setInterval(() => {
         this.currentIndex++;
+        this.changemove();
       }, 3000);
+    },
+    handleDom(){
+      const EleImg = this.$refs.swipers;
+      const EleBox = this.$refs.swiperBox;
+      if(EleImg.length>1) {
+        const FirstCloe = EleImg[0].cloneNode(true);
+        const LastClone = EleImg[EleImg.length-1].cloneNode(true);
+        EleBox.insertBefore(LastClone, EleImg[0]);
+        EleBox.appendChild(FirstCloe);
+        this.swiperStyle = EleBox.style;
+        this.totalWidth = EleBox.clientWidth;
+        // 滑动到正确的位置
+        this.setTransform(-this.totalWidth);
+      }
+    },
+    setTransform(position){
+     this.swiperStyle.transform = `translateX(${position}px)`;
+    },
+    changemove(){
+      const curLocation = -this.totalWidth*this.currentIndex;
+      this.changAmin().then(() =>{
+          console.log(this.currentIndex);
+          if(this.currentIndex >= this.$refs.swipers.length) {
+           this.currentIndex = 1;
+         }
+       });
+       this.setTransform(curLocation);
+    },
+    changAmin(){
+      return new Promise(()=>{
+          this.swiperStyle.transition = "all 1s ease";
+      })
     }
   }
 }
