a0d1ccb64c6ea233caac07f08cbdc1dcf34db299
wzc3867 <799015924@qq.com>
2022-02-21 00:02:46 +0800

    完成轮播图

--- src/components/common/swiper/swiper.vue
+++ src/components/common/swiper/swiper.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
   <div class="Contain">
-    <div class="swiper-box" ref="swiperBox">
+    <div class="swiper-box" ref="swiperBox" @touchstart="TouchStart" @touchmove="TouchMove" @touchend="TouchEnd">
     <img :src="item.image" v-for="(item, index) in imgswip" :key="index" ref="swipers" >
   </div>
    <!-- 指示点渲染 -->
@@ -29,19 +29,21 @@ export default {
   },
   data () {
     return {
-      currentIndex:1,
-      swiperStyle:{},
-      totalWidth:0,
-      indicatorIndex:1,
-      Timer:''
+      currentIndex:1,//当前的索引值
+      swiperStyle:{},//轮播组件样式
+      totalWidth:0,//轮播图片宽度
+      indicatorIndex:1,//轮播下面的指示点
+      Timer:'',//轮播滚动的定时器
+      Scrolling:false, //判断是否正在滑动的标志位
+      startX:'',//手指触碰的X轴坐标
+      currentX:'',//当前位置的X坐标
+      distance:'',//移动的距离
+      moveRatio:0.25//判断手指移动图片几分之几就要切换图片
     };
   },
   components: {},
 
   computed: {
-    move(){
-      return 'transform:translateX('+this.currentIndex*-375+'px';
-    },
     // 判断是否为当前类名
     ISactive(){
       try{
@@ -60,6 +62,7 @@ export default {
         this.changemove();
       }, 3000);
     },
+    // 操作DOM节点
     handleDom(){
       const EleImg = this.$refs.swipers;
       const EleBox = this.$refs.swiperBox;
@@ -74,10 +77,14 @@ export default {
         this.setTransform(-this.totalWidth);
       }
     },
+    // 设置位置的标志为
     setTransform(position){
      this.swiperStyle.transform = `translateX(${position}px)`;
     },
+    //轮播移动的方法
     changemove(){
+      this.Scrolling = true;
+      // console.log('scroll'+this.Scrolling);
       const curLocation = -this.totalWidth*this.currentIndex;
       // 滑动动画
        this.changAmin();
@@ -85,14 +92,20 @@ export default {
        this.setTransform(curLocation);
       //  检查滑动的位置
       this.checkLoacation()
+      
+      // console.log('scrolling1'+this.Scrolling);
     },
-      changAmin(){
+    //轮播移动的动画
+    changAmin(){
       this.swiperStyle.transition = "1s";
     },
     // 检查轮播图的位置
     checkLoacation(){
+      //判断下面的轮播点是否与图片对应上
       if(this.indicatorIndex >=this.$refs.swipers.length+1) {
         this.indicatorIndex =1;
+      } else if(this.indicatorIndex == 0){
+        this.indicatorIndex =4
       }
       // setTimeout(()=>{
       //   // 1.校验正确的位置
@@ -103,14 +116,23 @@ export default {
       //     }
       //     // this.$emit('transitionEnd',this.currentIndex-1)
       // },1200)
-      const that = this;
+      //保存VUE指向
+       const that = this;
+       //等待轮播过度动画完成来判断是否为最后一张图片或者第一张图片，如果是则立刻变动位置
        this.$refs.swiperBox.addEventListener('transitionend',function(){
+          that.Scrolling = false
+          // console.log('111'+that.Scrolling);
              // 1.校验正确的位置
           that.swiperStyle.transition = '0ms';
           if(that.currentIndex >= that.$refs.swipers.length+1) {
             that.currentIndex =1;
             that.setTransform(-that.currentIndex*that.totalWidth)
+          } else if(that.currentIndex == 0 && that.indicatorIndex == 4) {
+            // console.log(22);
+            that.currentIndex = that.$refs.swipers.length;
+            that.setTransform(-that.currentIndex*that.totalWidth)
           }
+          //  console.log('222'+that.Scrolling);
         })
     },
     changeImg(index){
@@ -130,12 +152,8 @@ export default {
         that.indicatorIndex = 1;
         this.staatTime();
         return;
-      } else {
-         this.$refs.swiperBox.addEventListener('transitionend',function(){
-            console.log(3);
-        })
-        position = -(num*this.totalWidth);
-      }
+      } 
+      position = -(num*this.totalWidth);
       // console.log(position);
       this.indicatorIndex = num;
       this.currentIndex = num;
@@ -143,7 +161,54 @@ export default {
       this.changAmin();
       this.setTransform(position);
       this.staatTime();
-    }
+    },
+    TouchStart(e) {
+      console.log('4444'+this.Scrolling);
+      if(this.Scrolling) {
+        clearInterval(this.Timer);
+        return false;
+      } 
+      console.log("我进来了吗？");
+      // e.preventDefault();
+      clearInterval(this.Timer);
+     // 3.保存开始滚动的位置
+        this.startX = e.touches[0].pageX;
+    },
+    TouchMove(e) {
+      //  if(this.Scrolling) {
+      //   return false;
+      // } 
+      this.currentX = e.touches[0].pageX;
+      this.distance = this.currentX - this.startX;
+      const currloaction = -this.currentIndex*this.totalWidth + this.distance;
+      this.setTransform(currloaction);
+    },
+    TouchEnd(e) {
+      let movedistance = Math.abs(this.distance);
+      if(this.distance == 0) {
+        return;
+      } else if(this.distance <0 && movedistance > this.totalWidth*this.moveRatio) {
+          this.currentIndex++;
+          this.indicatorIndex++;
+          console.log(this.Timer);
+          this.changAmin();
+          this.setTransform(-this.currentIndex*this.totalWidth);
+          this.checkLoacation();
+          this.staatTime();
+      } else if(this.distance >0 && movedistance > this.totalWidth*this.moveRatio) {
+          this.currentIndex--;
+          this.indicatorIndex--;
+          this.changAmin();
+          this.setTransform(-this.currentIndex*this.totalWidth);
+          this.checkLoacation();
+          this.staatTime();
+      } else{
+          this.changAmin();
+          this.setTransform(-this.currentIndex*this.totalWidth);
+          this.checkLoacation();
+          this.staatTime();
+      }
+    },
   }
 }
 
