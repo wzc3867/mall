2d40de2d12d0e93e7cf9b1951df657bfcbf56cc8
wzc3867 <799015924@qq.com>
2022-03-20 23:41:25 +0800

    提交商品列表开发

--- src/views/home/childrenCom/GoodList.vue
+++ src/views/home/childrenCom/GoodList.vue
@@ -5,7 +5,11 @@
           <li v-for="(item, index) in GoodData" :key="index" class="imgBox">
               <!-- {{item}} -->
             <img :src="item.show.img">
-    
+            <div class="img-text" v-for="(items) in item.props" :key="items" >{{items}}</div>
+            <div class="pricebox">
+                <span class="price">￥{{item.price}}</span>
+                <span class="collect"></span>
+            </div>
           </li>
       </ul>
   </div>
@@ -33,7 +37,8 @@ export default {
       },200)
   },
 
-  computed: {},
+  computed: {
+  },
 
   methods: {}
 }
@@ -53,12 +58,44 @@ export default {
     //     background-color:red
     // }
     .imgBox{
-        background-color:red;
+        // background-color:red;
         // height: 100px;
         width: 50%;
+        padding-top:1rem;
+        // position: relative;
     }
     .imgBox img {
-        width: 100%;
-        height: 100%;
+        width: 90%;
+        height: 90%;
+        margin-left:50%;
+        // text-align: center;
+        // position: absolute;
+        // left: 50%;
+        transform: translateX(-50%);
+    }
+    .img-text{
+        // display: -webkit-box
+        white-space: nowrap;
+        overflow: hidden;
+        font-size:12px;
+        text-overflow: ellipsis;
+        width:11rem;
+        margin-left:.7rem;
+    }
+    .price{
+        color:@color-tint;
+    }
+    .pricebox {
+        font-size:.8rem;
+        position: relative;
+    }
+    .collect::before{
+        position: absolute;
+         width: 14px;
+        height: 14px;
+        //  left: -15px;
+        top: 0;
+        content:'';
+        background: url('../../../assets/img/common/collect.svg') 100% 100%;
     }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,19 @@ module.exports = {
         // path.resolve(__dirname, './src/assets/reset.less')
         path.resolve(__dirname, 'src/assets/css/base.less')
       ]
-    }
+    },
+    chainWebpack: (config) => {
+      // config.resolve.alias
+      //     .set('@', resolve('src')) //3.0的写法 映射路径 @代表src 比如要写一个图片 <img src='@/assets/img/1.jpg'/>就会读取src下assets下img下的图片
+      const svgRule = config.module.rule('svg');
+      svgRule.uses.clear();
+      svgRule
+          .test(/\.svg$/)
+          .use('svg-sprite-loader')
+          .loader('svg-sprite-loader')
+          .options({
+              symbolId: 'icon-[name]'
+          });
+  },
   }
 };
